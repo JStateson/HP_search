f7cb35eb9521b5eaff1739ca41f788d6b54c41ec
Joseph Stateson <josephy@stateson.net>
2023-12-10 06:56:26 -0600

    Added the PartSurfer site
    The PartSurfer pops up in addition to the HP computing page.

--- background.js
+++ background.js
@@ -13,23 +13,23 @@
 // Add a listener to create the initial context menu items,
 // context menu items only need to be created at runtime.onInstalled
 chrome.runtime.onInstalled.addListener(async () => {
-  //for (const [tld, locale] of Object.entries(tldLocales)) {
     chrome.contextMenus.create({
       id: 'HP',
       title: 'HPsearch',
       type: 'normal',
       contexts: ['selection']
     });
-  //}
 });
 
-// Open a new search tab when the user clicks a context menu
+
 chrome.contextMenus.onClicked.addListener((item, tab) => {
-  //const tld = item.menuItemId;
-    const url = new URL(`https://support.hp.com/us-en/deviceSearch`);
-    url.searchParams.set('q', item.selectionText);
-    url.searchParams.append('origin','pdp');
-  chrome.tabs.create({ url: url.href, index: tab.index + 1 });
+    const url1 = new URL(`https://support.hp.com/us-en/deviceSearch`);
+    url1.searchParams.set('q', item.selectionText);
+    url1.searchParams.append('origin','pdp');
+    chrome.tabs.create({ url: url1.href, index: tab.index + 1 });
+    const url2 = new URL(`https://partsurfer.hp.com/partsurfer`);
+    url2.searchParams.set('searchtext', item.selectionText);
+    chrome.tabs.create({ url: url2.href, index: tab.index + 1 });
 });
 
 
